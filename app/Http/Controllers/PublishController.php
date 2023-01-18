802d851b119a23ce7ca70f41373f25f5e1445bc0
Masoud <gharagozlu.masoud@gmail.com>
2023-01-18 18:26:07 +0330

    Create project

---
+++ app/Http/Controllers/PublishController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Redis;
+
+class PublishController extends Controller
+{
+    public function publish()
+    {
+        Redis::publish('create-item', json_encode([
+            'name' => 'Book 1',
+            'author' => 'Masoud Gharagozlu'
+        ]));
+    }
+}
